82c08341913e46e8273e5250eba29f4cfeaac008
jisam <jisamcom@gmail.com>
2026-06-05 13:29:23 +0900

    feat: add messaging constants

---
+++ src/lib/messaging.js
@@ -0,0 +1,10 @@
+// Message type constants shared between background, content, options, popup.
+export const MSG = {
+  GET_SETTINGS: 'GET_SETTINGS',
+  SAVE_SETTINGS: 'SAVE_SETTINGS',
+  FETCH_FONT: 'FETCH_FONT',
+  APPLY_CSS: 'APPLY_CSS',
+  REMOVE_CSS: 'REMOVE_CSS',
+  TOGGLE_SITE: 'TOGGLE_SITE',
+  REAPPLY: 'REAPPLY',
+};
